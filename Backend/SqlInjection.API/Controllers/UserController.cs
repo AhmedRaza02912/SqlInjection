4f5197a1f3f17d66f8f7076c3d5da5112cf1e21e
ahmed03 <142502232+AhmedRaza02912@users.noreply.github.com>
2026-07-14 10:20:36 +0500

    Added SqlInterporlated method

--- Backend/SqlInjection.API/Controllers/UserController.cs
+++ Backend/SqlInjection.API/Controllers/UserController.cs
@@ -19,6 +19,8 @@ public class UserController : ControllerBase
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest request)
     {
+        // OR 1=1-- is treated as text not executable SQL
+        // It becomes WHERE Username = @__request_Username_0 (parameters)
         var user = await _db.Users.FirstOrDefaultAsync(
             u => u.Username == request.Username && u.Password == request.Password);
 
@@ -33,6 +35,24 @@ public class UserController : ControllerBase
         }
         return Ok(new {message = $"Welcome {user.Username}"});
         
+    }
+    [HttpPost("login-raw-safe")]
+    public async Task<IActionResult> LoginRawSafe(LoginRequest request)
+    { //FromSqlInterpolated allows use of raw sql queries
+    // while parameterizing the variables to mitigate SQL Injection
+        var user = await _db.Users.FromSqlInterpolated(
+            $@"SELECT * FROM Users WHERE Username = {request.Username}
+            AND Password = {request.Password}").FirstOrDefaultAsync();
+
+            if(user == null)
+        {
+            return Unauthorized(new {message="Invalid Username or password"});
+        }
+        
+        return Ok(new
+        {
+            message = $"Welcome {user.Username}"
+        });
     }
 
 }
